7705fbb991eb5f0d8722d43056d3773f6db006f8
Schmutz Philip taschmut <philip.schmutz@stud.hslu.ch>
2019-05-12 23:23:30 +0200

    changed api key

--- etl-lite/files-fetch.sh
+++ etl-lite/files-fetch.sh
@@ -12,37 +12,37 @@ test -d ./data || mkdir ./data
 
 ## ##################################################
 ## get data
-curl -sSLG "https://api.airvisual.com/v2/city?city=Bern&state=Bern&country=Switzerland&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/switzerland.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Berlin&state=Berlin&country=Germany&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/germany.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Brussels&state=brussels-capital&country=Belgium&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/belgium.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Escaldes-Engordany&state=Escaldes-Engordany&country=Andorra&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/andorra.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Vienna&state=Vienna&country=Austria&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/austria.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Sarajevo&state=Federation-of-B-H&country=Bosnia-Herzegovina&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/bosnia.json
+curl -sSLG "https://api.airvisual.com/v2/city?city=Bern&state=Bern&country=Switzerland&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/switzerland.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Berlin&state=Berlin&country=Germany&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/germany.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Brussels&state=brussels-capital&country=Belgium&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/belgium.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Escaldes-Engordany&state=Escaldes-Engordany&country=Andorra&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/andorra.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Vienna&state=Vienna&country=Austria&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/austria.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Sarajevo&state=Federation-of-B-H&country=Bosnia-Herzegovina&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/bosnia.json
 sleep 1m
-curl -sSLG "https://api.airvisual.com/v2/city?city=Sofia&state=Sofia&country=Bulgaria&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/bulgaria.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Nicosia&state=Nicosia&country=Cyprus&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/cyprus.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Prague&state=Praha&country=Czech-Republic&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/czechrepublic.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Copenhagen&state=Capital-Region&country=Denmark&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/denmark.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Helsinki&state=Uusimaa&country=Finland&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/finland.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Paris&state=Ile-de-France&country=France&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/france.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Sofia&state=Sofia&country=Bulgaria&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/bulgaria.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Nicosia&state=Nicosia&country=Cyprus&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/cyprus.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Prague&state=Praha&country=Czech-Republic&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/czechrepublic.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Copenhagen&state=Capital-Region&country=Denmark&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/denmark.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Helsinki&state=Uusimaa&country=Finland&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/finland.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Paris&state=Ile-de-France&country=France&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/france.json 
 sleep 1m
-curl -sSLG "https://api.airvisual.com/v2/city?city=Budapest&state=Central-Hungary&country=Hungary&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/hungary.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Dublin&state=Leinster&country=Ireland&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/ireland.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=VEnice&state=Veneto&country=Italy&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/italy.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Riga&state=Riga&country=Latvia&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/latvia.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Vilnius&state=Vilnius&country=Lithuania&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/lithuania.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Budapest&state=Central-Hungary&country=Hungary&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/hungary.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Dublin&state=Leinster&country=Ireland&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/ireland.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=VEnice&state=Veneto&country=Italy&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/italy.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Riga&state=Riga&country=Latvia&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/latvia.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Vilnius&state=Vilnius&country=Lithuania&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/lithuania.json 
 sleep 1m
-curl -sSLG "https://api.airvisual.com/v2/city?city=Esch-sur-Alzette&state=district-de-Luxembourg&country=Luxembourg&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/luxembourg.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Skopje&state=Skopje&country=Macedonia&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/macedonia.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Amsterdam&state=North-Holland&country=Netherlands&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/netherlands.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Oslo&state=Oslo&country=Norway&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/norway.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=warsaw&state=mazovia&country=Poland&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/poland.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Lisbon&state=Lisbon&country=Portugal&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/portugal.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Bucharest&state=Bucuresti&country=Romania&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/romania.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Esch-sur-Alzette&state=district-de-Luxembourg&country=Luxembourg&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/luxembourg.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Skopje&state=Skopje&country=Macedonia&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/macedonia.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Amsterdam&state=North-Holland&country=Netherlands&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/netherlands.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Oslo&state=Oslo&country=Norway&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/norway.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=warsaw&state=mazovia&country=Poland&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/poland.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Lisbon&state=Lisbon&country=Portugal&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/portugal.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Bucharest&state=Bucuresti&country=Romania&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/romania.json 
 sleep 1m
-curl -sSLG "https://api.airvisual.com/v2/city?city=Bratislava&state=Bratislava&country=Slovakia&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/slovakia.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Madrid&state=Madrid&country=Spain&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/spain.json 
-curl -sSLG "api.airvisual.com/v2/city?city=Gavle&state=Gaevleborg&country=Sweden&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/sweden.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Ankara&state=Ankara&country=Turkey&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/turkey.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=Kyiv&state=Kyiv&country=Ukraine&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/ukraine.json 
-curl -sSLG "https://api.airvisual.com/v2/city?city=London&state=England&country=UK&key=LndE4cvvmM6A8LL2f" | jq  -c '.'  >data/uk.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Bratislava&state=Bratislava&country=Slovakia&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/slovakia.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Madrid&state=Madrid&country=Spain&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/spain.json 
+curl -sSLG "api.airvisual.com/v2/city?city=Gavle&state=Gaevleborg&country=Sweden&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/sweden.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Ankara&state=Ankara&country=Turkey&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/turkey.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=Kyiv&state=Kyiv&country=Ukraine&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/ukraine.json 
+curl -sSLG "https://api.airvisual.com/v2/city?city=London&state=England&country=UK&key=Ny6g2Tc8KirnBMytX" | jq  -c '.'  >data/uk.json 
